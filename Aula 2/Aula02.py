e701d287b73ef354886971a3b2f88afa1781b2b2
vanesouzan <Vanessa2403200@gmail.com>
2024-12-02 10:00:45 -0300

    Aprendi oque é uma variavél

---
+++ Aula 2/Aula02.py
@@ -0,0 +1,8 @@
+print('olá,mundo')
+
+nome_usuario= 'Vanessa Santos' #tipo-> str =string
+idade_usuario=19#tipo->int=interager
+altura_usuario=1.65 #tipo->float
+maior_de_idade= True #tipo->bool=boolean
+
+print('seja bem-vindo',nome_usuario, 'sua idade é',idade_usuario,'sua altura é',altura_usuario,'voce é maior de idade',maior_de_idade)
